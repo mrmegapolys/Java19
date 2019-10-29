404cec7af4c6e073e2de66ce028abfab5a6fc5d6
Igor <rozhkovigor@yandex.ru>
2019-10-29 15:15:21 +0300

    added homework_08

---
+++ homework_08/src/TaskRunner.java
@@ -0,0 +1,12 @@
+public class TaskRunner<T> implements Runnable{
+    private final Task<T> task;
+
+    public TaskRunner(Task<T> task) {
+        this.task = task;
+    }
+
+    @Override
+    public void run() {
+        System.out.println(task.get());
+    }
+}
