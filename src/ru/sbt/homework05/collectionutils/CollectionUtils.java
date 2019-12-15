baa7b4d83f2262a5f03e8e0c9905cb79f8fef691
Igor <rozhkovigor@yandex.ru>
2019-12-15 19:16:40 +0300

    added CollectionUtils

--- src/ru/sbt/homework05/collectionutils/CollectionUtils.java
+++ src/ru/sbt/homework05/collectionutils/CollectionUtils.java
@@ -3,47 +3,66 @@ package ru.sbt.homework05.collectionutils;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
+import java.util.function.Predicate;
 
 public class CollectionUtils {
     public static <T> void addAll(List<? extends T> source, List<? super T> destination) {
         destination.addAll(source);
     }
 
-    public static List newArrayList() {
-        return new ArrayList();
+    public static <T> List<T> newArrayList() {
+        return new ArrayList<>();
     }
 
-    public static int indexOf(List source, Object o) {
-        return 0;
+    public static <T> int indexOf(List<? super T> source, T object) {
+        int counter = 0;
+        for (Object elem : source) {
+            if (elem.equals(object)) return counter;
+            counter++;
+        }
+        return -1;
     }
 
-    public static List limit(List source, int size) {
-        return null;
+    public static <T> List<T> limit(List<T> source, int size) {
+        List<T> result = CollectionUtils.newArrayList();
+        int counter = 0;
+        for (T elem : source) {
+            if (counter >= size) break;
+            result.add(elem);
+            counter++;
+        }
+        return result;
     }
 
-
-    public static void add(List source, Object o) {
-
+    public static <T> void add(List<? super T> source, T elem) {
+        source.add(elem);
     }
 
-    public static void removeAll(List removeFrom, List c2) {
-
+    public static <T> void removeAll(List<? super T> removeFrom, List<? extends T> c2) {
+        c2.forEach(removeFrom::remove);
     }
 
-    public static boolean containsAll(List c1, List c2) {
-        return false;
+    public static <T> boolean containsAll(List<? super T> c1, List<? extends T> c2) {
+        return c1.containsAll(c2);
     }
 
-    public static boolean containsAny(List c1, List c2) {
-        return false;
+    public static <T> boolean containsAny(List<? super T> c1, List<? extends T> c2) {
+        return c2.stream().anyMatch(c1::contains);
     }
 
-    public static List range(List list, Object min, Object max) {
-        return null;
+    public static <T extends Comparable<? super T>> List<T> range(List<? extends T> list, T min, T max) {
+        return range(list, elem -> elem.compareTo(min) > 0 && elem.compareTo(max) < 0);
     }
 
-    public static List range(List list, Object min, Object max, Comparator comparator) {
-        return null;
+    public static <T> List<T> range(List<? extends T> list, T min, T max, Comparator<? super T> comparator) {
+        return range(list, elem -> comparator.compare(elem, min) > 0 && comparator.compare(elem, max) < 0);
     }
 
+    private static <T> List<T> range(List<? extends T> list, Predicate<T> predicate) {
+        List<T> result = CollectionUtils.newArrayList();
+        list.stream()
+                .filter(predicate)
+                .forEach(result::add);
+        return result;
+    }
 }
